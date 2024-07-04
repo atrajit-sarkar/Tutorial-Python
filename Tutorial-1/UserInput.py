368501219ab3dbaf17bf66289775dee641b2d6a4
atrajit-sarkar <atrajit.sarkar@gmail.com>
2024-07-04 11:36:53 +0530

    update

---
+++ Tutorial-1/UserInput.py
@@ -0,0 +1,33 @@
+# Taking Input from User
+# x=input("Enter your number: ")
+# print(f"Your integer is : {x}")
+
+# Conditional Statement(Calculator)
+
+# x=int(input("Enter your num: "))
+# y=int(input("Enter your num: "))
+# z=input("Enter your num: ")
+
+# if z=="+":
+#     print(f"your output is: {x+y}")
+
+# elif z=="-":
+#     print(f"your output is: {x-y}")
+
+# elif z=="*":
+#     print(f"your output is: {x*y}")
+
+# elif z=="/":
+#     print(f"your output is: {x/y}")
+
+# elif z=="**":
+#     print(f"your output is: {x**y}")
+
+
+# Stone-Paper-Sissor
+
+x=input("Player1: ")
+y=input("Plaayer2: ")
+
+if x=="stone" and y=="paper":
+    print("Player2 wins...")
